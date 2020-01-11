34d6061735ae9edb3f44cdb8452b27201375abe0
flaviofernandes004 <flaviofernandes004@gmail.com>
2020-01-11 19:00:01 +0000

    Add support for oidc

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3-alpine
 
 RUN apk update \
-      && apk add --no-cache python3-dev build-base linux-headers pcre-dev curl uwsgi \
+      && apk add --no-cache python3-dev build-base linux-headers pcre-dev libffi-dev openssl-dev curl uwsgi \
       && curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 
 ENV PATH="/root/.poetry/bin:${PATH}"
@@ -9,9 +9,8 @@ ENV PATH="/root/.poetry/bin:${PATH}"
 WORKDIR /usr/src/
 COPY pyproject.toml pyproject.toml
 COPY poetry.lock poetry.lock
-
 RUN poetry version && poetry install
 
 COPY server.py server.py
 
-ENTRYPOINT ["poetry","run", "python","server.py"]
+CMD ["poetry","run", "python","server.py"]
--- server.py
+++ server.py
@@ -1,12 +1,140 @@
-from flask import Flask
+import json
+from flask_oidc_ex import OpenIDConnect
+from flask import Flask, g, jsonify, request, session
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
+app.config.from_json("config/config.json")
+oidc = OpenIDConnect(app)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+@app.route("/flask-oidc/")
+def home():
+    if oidc.user_loggedin:
+        email = oidc.user_getfield("email")
+        return f"""
+            <div>
+                <div>Hello, {email}</dic>
+                <ul>
+                    <li><a href="/flask-oidc/profile/">Profile</a></li>
+                    <li><a href="/flask-oidc/token_info/">token_info</a></li>
+                    <li><a href="/flask-oidc/role_protected/token_info/">role_token_info</a></li>
+                    <li><a href="/flask-oidc/logout/">Log out</a></li>
+                </ul>
+            </div>
+        """
+    else:
+        return 'Welcome anonymous, <a href="/flask-oidc/login/">Log in</a>'
+
+
+@app.route("/flask-oidc/login/")
+@oidc.require_login
+def login():
+    info = oidc.user_getinfo(["email"])
+    return 'Hello, %s! <a href="/flask-oidc/">Home</a>' % (info.get("email"))
+
+
+@app.route("/flask-oidc/profile/")
+@oidc.require_login
+def profile():
+    fields = [
+        "given_name",
+        "family_name",
+        "email",
+        "email_verified",
+        "roles",
+        "resouce_access",
+        "username",
+        "active",
+        "scopes",
+    ]
+    info = oidc.user_getinfo(fields)
+    return f"""
+            <div>
+                <a href="/flask-oidc/logout/">Log out</a>
+                <div>Profile: {info}</dic>
+            </div>
+        """
+
+
+@app.route("/flask-oidc/token_info/")
+@oidc.require_login
+@oidc.accept_token(require_token=False, scopes_required=["flask-oidc:read-token"])
+def token_info():
+    fields = [
+        "given_name",
+        "family_name",
+        "email",
+        "email_verified",
+        "roles",
+        "resouce_access",
+        "username",
+        "active",
+        "scopes",
+    ]
+    info = oidc.user_getinfo(fields)
+    return jsonify(info)
+
+
+@app.route("/flask-oidc/role_protected/token_info")
+@oidc.require_login
+@oidc.require_keycloak_role(client="flask-oidc-example", role="x-admin")
+def role_proctected_token_info():
+    fields = [
+        "given_name",
+        "family_name",
+        "email",
+        "email_verified",
+        "roles",
+        "resouce_access",
+        "username",
+        "active",
+        "scopes",
+    ]
+    info = oidc.user_getinfo(fields)
+    return jsonify(info)
+
+
+@app.route("/flask-oidc/api/role_protected/token_info")
+@oidc.require_keycloak_role(client="flask-oidc-example", role="x-admin")
+def api_role_proctected_token_info():
+    fields = [
+        "given_name",
+        "family_name",
+        "email",
+        "email_verified",
+        "roles",
+        "resouce_access",
+        "username",
+        "active",
+        "scopes",
+    ]
+    info = oidc.user_getinfo(fields)
+    return jsonify(info)
+
+
+@app.route("/flask-oidc/api/token_protected/token_info")
+@oidc.accept_token(require_token=True, scopes_required=["flask-oidc:read-token"])
+def api_token_proctected_token_info():
+    return jsonify(g.oidc_token_info)
+
+
+@app.route("/flask-oidc/api/role_and_token_protected/token_info")
+@oidc.accept_token(require_token=True, scopes_required=["flask-oidc:read-profile"])
+@oidc.require_keycloak_role(client="flask-oidc-example", role="x-admin")
+def api_role_and_token_proctected_token_info():
+    return jsonify(g.oidc_token_info)
+
+
+@app.route("/flask-oidc/logout/")
+def logout():
+    oidc.logout()
+    session.clear()
+    return 'Hi, you have been logged out! <a href="/flask-oidc/">Home</a>'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
